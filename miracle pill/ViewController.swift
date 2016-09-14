b2a4e0884364e36ab01ca9304446c8d4f40bf470
Duncan Hamilton <rtdcommerce@icloud.com>
2016-09-14 11:30:53 -0400

    Initial Commit

---
+++ miracle pill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miracle pill
+//
+//  Created by Duncan Hamilton on 2016-09-14.
+//  Copyright © 2016 Duncan Hamilton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
