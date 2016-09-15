31c28d4b04c4e50f02aec8ac6a9be3412386e9b0
Duncan Hamilton <rtdcommerce@icloud.com>
2016-09-15 11:48:33 -0400

    removed state picker view

--- miracle pill/ViewController.swift
+++ miracle pill/ViewController.swift
@@ -8,17 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-
-    @IBOutlet weak var statePicker: UIPickerView!
-    @IBOutlet weak var statePickerBtn: UIButton!
+class ViewController: UIViewController {
     
-    let states = ["Alaska,","Arkansas","Alabama","NewYork"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        statePicker.dataSource = self
-        statePicker.delegate = self
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -28,26 +22,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func stateBtnPressed(_ sender: AnyObject) {
-        statePicker.isHidden = false
-    }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-        
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return states.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return states[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
-        statePicker.isHidden = true
-    }
-}
+       }
+
 
